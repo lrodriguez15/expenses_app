61739ccce498f76eb5978c436bba78782b3d2c1c
Dr. Leo Rodriguez <leo@helgen.tech>
2023-01-25 18:13:03 -0600

    Initial Commit for Expenses App

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:expenses_app/widgets/transaction_list.dart';
+import 'package:flutter/material.dart';
+
+import './models/transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // late String titleInput;
+  // late String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('CHART!'),
+            ),
+          ),
+          TransactionList(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  const NewTransaction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
